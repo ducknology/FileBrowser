997450c307390a1c3760bee33fda504cbc610cd6
Billy <billy.chan@harborx.com>
2016-05-24 18:37:35 +0800

    Add delete function

--- FileBrowser/FileBrowser.swift
+++ FileBrowser/FileBrowser.swift
@@ -10,55 +10,64 @@ import Foundation
 
 /// File browser containing navigation controller.
 public class FileBrowser: UINavigationController {
-    
-    let parser = FileParser.sharedInstance
-    
-    var fileList: FileListViewController?
+	
+	let parser = FileParser.sharedInstance
+	
+	var fileList: FileListViewController?
+	
+	/// File types to exclude from the file browser.
+	public var excludesFileExtensions: [String]? {
+		didSet {
+			parser.excludesFileExtensions = excludesFileExtensions
+		}
+	}
+	
+	/// File paths to exclude from the file browser.
+	public var excludesFilepaths: [NSURL]? {
+		didSet {
+			parser.excludesFilepaths = excludesFilepaths
+		}
+	}
+	
+	/// Override default preview and actionsheet behaviour in favour of custom file handling.
+	public var didSelectFile: ((FBFile) -> ())? {
+		didSet {
+			fileList?.didSelectFile = didSelectFile
+		}
+	}
+	
+	public var allowDelete = false
+	
+	/**
+	Init to documents folder.
+	
+	- returns: File browser view controller.
+	*/
+	public convenience init() {
+		let parser = FileParser.sharedInstance
+		let path = parser.documentsURL()
+		self.init(initialPath: path)
+	}
+	
+	/**
+	Init to a custom directory path.
+	
+	- parameter initialPath: NSURL filepath to containing directory.
+	
+	- returns: File browser view controller.
+	*/
+	public convenience init(initialPath: NSURL) {
+		let fileListViewController = FileListViewController(initialPath: initialPath)
+		self.init(rootViewController: fileListViewController)
+		self.view.backgroundColor = UIColor.whiteColor()
+		self.fileList = fileListViewController
+		self.fileList?.allowDeleteDelegate = self
+	}
+}
 
-    /// File types to exclude from the file browser.
-    public var excludesFileExtensions: [String]? {
-        didSet {
-            parser.excludesFileExtensions = excludesFileExtensions
-        }
-    }
-    
-    /// File paths to exclude from the file browser.
-    public var excludesFilepaths: [NSURL]? {
-        didSet {
-            parser.excludesFilepaths = excludesFilepaths
-        }
-    }
-    
-    /// Override default preview and actionsheet behaviour in favour of custom file handling.
-    public var didSelectFile: ((FBFile) -> ())? {
-        didSet {
-            fileList?.didSelectFile = didSelectFile
-        }
-    }
-    
-    /**
-     Init to documents folder.
-     
-     - returns: File browser view controller.
-     */
-    public convenience init() {
-        let parser = FileParser.sharedInstance
-        let path = parser.documentsURL()
-        self.init(initialPath: path)
-    }
-    
-    /**
-     Init to a custom directory path.
-     
-     - parameter initialPath: NSURL filepath to containing directory.
-     
-     - returns: File browser view controller.
-     */
-    public convenience init(initialPath: NSURL) {
-        let fileListViewController = FileListViewController(initialPath: initialPath)
-        self.init(rootViewController: fileListViewController)
-        self.view.backgroundColor = UIColor.whiteColor()
-        self.fileList = fileListViewController
-    }
-    
+extension FileBrowser: FileListDeletableDelegate
+{
+	public func allowDeleteFile() -> Bool {
+		return self.allowDelete
+	}
 }
